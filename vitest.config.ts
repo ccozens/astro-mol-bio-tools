c8ecb490562d4c344d3630c410258a5500d3b2cb
ccozens <ccozens@gmail.com>
2023-03-05 13:24:05 +0000

    modified vitest to gather coverage from all component and function files in src (note: not.astro for now, only .ts(x))

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,24 @@
 /// <reference types="vitest" />
-import { getViteConfig } from 'astro/config';
+// import { getViteConfig } from 'astro/config';
+import { defineConfig } from 'vitest/config';
 
-export default getViteConfig({
+// export default getViteConfig({
+
+export default defineConfig({
 	test: {
 		/* for example, use global to avoid globals imports (describe, test, expect): */
 		globals: true,
-		watch: true
+		watch: true,
+		coverage: {
+			// include all files (meaning: including those without any tests written yet) in coverage calcs
+			all: true,
+			// exclude config files and tests from coverage calcs
+			exclude: [ 
+				"*.config.ts", 
+				"*/*.d.ts",
+				"test/**"
+			]
+		}
 	},
 });
 
