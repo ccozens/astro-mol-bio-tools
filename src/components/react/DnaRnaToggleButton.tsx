d6697676c62fd3b8ed320c3c317f42a1bf66d073
ccozens <ccozens@gmail.com>
2023-03-11 14:15:37 +0000

    added DNA/RNA toggle logic, encountered need to properly setup checkDnaRnaInput

--- src/components/react/DnaRnaToggleButton.tsx
+++ src/components/react/DnaRnaToggleButton.tsx
@@ -1,23 +1,21 @@
-import { useState, ChangeEvent } from 'react';
+import { dnaRnaStore } from '../../stores/dnaRnaStore';
+import type { ChangeEvent } from 'react';
+
 
 export default function DnaRnaToggleButton() {
-  const [switchState, setSwitchState] = useState(true);
+  
+  function handleToggle (e: ChangeEvent<HTMLInputElement>): void {
+    e.target.checked ? dnaRnaStore.set(false) : dnaRnaStore.set(true);
+  };
 
-  function handleToggle(e: ChangeEvent<HTMLInputElement>) {
-    console.log('before' + switchState);
-    setSwitchState(!switchState);
-    console.log('after' + switchState);
-  }
   return (
     <div className="dnaRnaToggleButton" id="dnaRnaToggleButton-1">
       <label>
         <input
           className="dnaRnaToggleCheckbox"
           type="checkbox"
-          checked={switchState}
           onChange={handleToggle}
         />
-
         <div className="knobs"></div>
         <div className="layer"></div>
       </label>
--- src/components/react/DnaWeight.tsx
+++ src/components/react/DnaWeight.tsx
@@ -1,4 +1,5 @@
 import { useStore } from '@nanostores/react';
+import { inputStore } from '../../stores/input';
 import {
   approxRnaMw,
   approxSsDnaMw,
@@ -7,28 +8,56 @@ import {
   exactSsDnaMw,
   exactDsDnaMw,
 } from '../../functions/dnaWeight';
-import { inputStore } from '../../stores/input';
+import { dnaRnaStore } from '../../stores/dnaRnaStore';
 import DnaRnaToggleButton from './DnaRnaToggleButton';
-
+import { Molecule } from '../../types';
+import { checkDnaInput } from '../../functions/checkDnaInput';
 interface LabelProps {
   ariaLabelContent: string;
 }
 
 export default function DnaWeight({ ariaLabelContent }: LabelProps) {
+  // data from input
   const sanitisedInputFromStore = useStore(inputStore);
+      // check input
+      const checkedInput = checkDnaInput(sanitisedInputFromStore, Molecule.Dna);
+      if (checkedInput.includes('Non-DNA')) {
+         return <div className="outputBox">
+                  {checkedInput}
+                </div> }
 
-  const calcApproxRnaMw = approxRnaMw(sanitisedInputFromStore);
-  const calcApproxSsDnaMw = approxSsDnaMw(sanitisedInputFromStore);
-  const calcApproxDsDnaMw = approxDsDnaMw(sanitisedInputFromStore);
-  const calcExactRnaMw = exactRnaMw(sanitisedInputFromStore);
-  const calcExactSsDnaMw = exactSsDnaMw(sanitisedInputFromStore);
-  const calcExactDsDnaMw = exactDsDnaMw(sanitisedInputFromStore);
+
+  // DNA / RNA toggle
+  const isDna = useStore(dnaRnaStore);
+
+  let calcApproxSsDnaMw: number = 0;
+  let calcApproxDsDnaMw: number = 0;
+  let calcExactSsDnaMw: number = 0;
+  let calcExactDsDnaMw: number = 0;
+  let calcApproxRnaMw: number = 0;
+  let calcExactRnaMw: number = 0;
+
+  if (isDna) {
+    calcApproxSsDnaMw = approxSsDnaMw(sanitisedInputFromStore);
+    calcApproxDsDnaMw = approxDsDnaMw(sanitisedInputFromStore);
+    calcExactSsDnaMw = exactSsDnaMw(sanitisedInputFromStore);
+    calcExactDsDnaMw = exactDsDnaMw(sanitisedInputFromStore);
+  }
+  if (!isDna) {
+    calcApproxRnaMw = approxRnaMw(sanitisedInputFromStore);
+    calcExactRnaMw = exactRnaMw(sanitisedInputFromStore);
+  }
 
   return (
     <div className="innerOutputBox" aria-label={ariaLabelContent}>
       <p className="countHeading">Approx weight</p>
       <DnaRnaToggleButton />
-      
+      <p className="countItem">{isDna && Number(calcApproxSsDnaMw).toFixed(2)}</p>
+      <p className="countItem">{isDna && Number(calcApproxDsDnaMw).toFixed(2)}</p>
+      <p className="countItem">{isDna && Number(calcExactSsDnaMw).toFixed(2)}</p>
+      <p className="countItem">{isDna && Number(calcExactDsDnaMw).toFixed(2)}</p>
+      <p className="countItem">{!isDna && Number(calcApproxRnaMw).toFixed(2)}</p>
+      <p className="countItem">{!isDna && Number(calcExactRnaMw).toFixed(2)}</p>
       <p className="weightItem"></p>
     </div>
   );
